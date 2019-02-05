d08bc7ac1d01cb616b0ae66eac7104e4d45fcb35
jaumeortola <jaumeortola@gmail.com>
2019-02-05 09:52:34 +0000

    rm file not used

--- script-spa-cat/run.sh
+++
@@ -1,7 +0,0 @@
-python3 main.py < "plantilla-traduccions - traduccions.tsv" > output.txt
-uniq cat.dix > cat2.dix
-mv cat2.dix cat.dix
-uniq spa.dix > spa2.dix
-mv spa2.dix spa.dix
-uniq spa-cat.dix > spa-cat2.dix
-mv spa-cat2.dix spa-cat.dix
